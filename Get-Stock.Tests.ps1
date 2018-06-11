6f400c7a3bfb26946e290165f89e1a72c13f407d
Tyler Wright <tylwright@gmail.com>
2018-06-10 21:29:49 -0400

    Added select option to return specific value instead of full report

--- Get-Stock.Tests.ps1
+++ Get-Stock.Tests.ps1
@@ -17,9 +17,26 @@ Describe "Get-Stock -Symbol F" {
         It "CEO is James P. Hackett" {
             Get-Stock -Symbol F | Out-String | Should -BeLike '*James P. Hackett*'
         }
-       It "Industry is Autos" {
+        It "Industry is Autos" {
             Get-Stock -Symbol F | Out-String | Should -BeLike '*Autos*'
        }
+    }
+}
+
+Describe "Get-Stock -Symbol AAPL -Select <key>" {
+    Context "Check results" {
+        It "CEO is Timothy D. Cook" {
+            Get-Stock -Symbol AAPL -Select CEO | Should -Be 'Timothy D. Cook'
+        }
+        It "Company is Apple Inc." {
+            Get-Stock -Symbol AAPL company | Should -Be "Apple Inc."
+        }
+        It "Website is http://www.apple.com" {
+            Get-Stock -Symbol AAPL -Select website | Should -Be 'http://www.apple.com'
+        }
+        It "Industry is Computer Hardware" {
+            Get-Stock -Symbol AAPL -Select industry | Should -Be 'Computer Hardware'
+        }
     }
 }
 
@@ -38,6 +55,9 @@ Describe "Errors" {
     Context "Stop and raise error if..." {
         It "Symbol could not be found = Error" {
             { Get-Stock -Symbol tylwright -ErrorAction Stop } | Should -Throw "Symbol (tylwright) could not be found."
+        }
+        It "Passed incorrect 'select' argument" {
+            { Get-Stock -Symbol AAPL -Select tylwright -ErrorAction Stop } | Should -Throw "Select options: price, openingPrice, closingPrice, CEO, industry, website, company"
         }
     }
 }
